cd40a010a4a027e9a7ab04f11c1a25aacb68f7b6
dicoxx <1150621366@qq.com>
2022-07-01 10:32:50 +0800

    update

--- FileOperate.cpp
+++ FileOperate.cpp
@@ -4,19 +4,20 @@
 
 using namespace std;
 
+
+
+
 void FileOperate::GenToFPKM(string srcFile, string dstFile)
 {
-	//´´½¨ÎÄ¼şÁ÷
 	ifstream inFile;
 	inFile.open(srcFile, ios::in | ios::out);
 	string str;
-	vector<vector<string>>num;  // vectorÈİÆ÷ÓÃÀ´´æ·ÅÊı¾İ£¨Ã¿Ò»ĞĞÊı¾İÎªÒ»¸övector£©£¬Í³Ò»°´stringĞÍÊı¾İ¶ÁÈ¡
+	vector<vector<string>>num;  // vectorÂ¿Â¿Â¿4Â´Â¿Â¿Â¾Â¿Â¨Ã¿Â¿Â¿Â¿Â¾Â¿ÂªÂ¿Â¸Â¿Â¿torÂ£Â©Â£Â¬Â¿Â¿Â°Â´stringÂ¿Â¿Â¾Â¿Â¡
 
-	//  °´ĞĞ¶ÁÈ¡ÎÄ¼ş
 	while (getline(inFile, str))
 	{
 		istringstream input(str);
-		vector<string> tmp;  // vectorÓÃÀ´´æ·ÅÃ¿ĞĞÊı¾İ
+		vector<string> tmp;  // vectorÂ¿4Â´Â¿Ã¿Â¿Â¿Â¾
 		string a;
 		while (input >> a)
 		{
@@ -24,41 +25,41 @@ void FileOperate::GenToFPKM(string srcFile, string dstFile)
 		}
 		num.push_back(tmp);
 	}
-	
-	float sum[21] = { 0 }; // sumÊı×éÓÃÀ´´æ·ÅÁĞºÍ  ±ãÓÚºóĞøFPKM¼ÆËã
-	for (int j = 0; j < sizeof(sum) / sizeof(sum[0]); j++)
+
+	float *sum=new float[num.size()];//[SUMARRSIZE] = { 0 }; 
+	for (int i =2; i < num[i].size(); i++)
 	{
-		for (int i = 1; i < num.size(); i++)
+		for (int j = 1; j < num.size(); j++)
 		{
-			float aa =(float)atof( num[i][j + 2].c_str());  // ½«vector¶Áµ½µÄÊı¾İ×ªÎªfloatĞÍ
-			sum[j] += aa;
+			float aa = (float)atof(num[j][i].c_str());  // Â½Â«vectorÂ¶uÂ½ÂµÂ¿Â¿Â¿ÂªfloatÂ¿
+			sum[i-2] += aa;
 		}
 	}
 
-	// ¶Ô³ı±íÍ·ÒÔÍâµÄÊı¾İ½øĞĞ²Ù×÷
+	// Â¶Â¿Â¿Â¿Â¿Â¿Â¿Â¾Â¿Â¿Â¿Â¿
 	for (int i = 1; i < num.size(); i++)
 	{
 		for (int j = 2; j < num[i].size(); j++)
-		{   
+		{
 			float a = (float)atof(num[i][j].c_str());
 			float lenNum = (float)atof(num[i][1].c_str());
 			float db2 = a * 1000 * 1000000 / (lenNum * sum[j - 2]);  // FPKM
 			ostringstream oss;
-			oss.setf(ios::fixed);  // °´¹Ì¶¨µã·½Ê½ÏÔÊ¾¸¡µãÊı
-			oss.precision(20);  // ±£Áô20Î»Ğ¡Êı
-			
+			oss.setf(ios::fixed);  // Â°Â´Â¹Â¿Â¨ÂµÂ¿Â¿Â¿Â¿Â¸Â¡ÂµÂ¿
+			oss.precision(20);  // Â±Â£Â´20Â¿Â¿Â¿
+
 			oss << db2;
 
 			string str(oss.str());
 			num[i][j] = str;
 		}
-		
+
 	}
 
 	std::ofstream outFile;
-	//  ´ò¿ªÎÄ¼ş
+	//  Â´Â¿Â¿Â¿Ã¾
 	outFile.open(dstFile);
-	//  ½«vectorÖĞÊı¾İĞ´ÈëÎÄ±¾
+	//  Â½Â«vectorÂ¿Â¿Â¾Â¿Â´Â¿Â¿Â±Â¾
 	for (int i = 0; i < num.size(); i++)
 	{
 		for (int j = 0; j < num[i].size(); j++)
@@ -73,65 +74,77 @@ void FileOperate::GenToFPKM(string srcFile, string dstFile)
 
 	inFile.close();
 	outFile.close();
+
 }
-void FileOperate::GenToTPM(string srcFile, string dstFile)
+void FileOperate::GenToTPM(string srcFile, string dstFile )
 {
 	ifstream inFile;
 	inFile.open(srcFile, ios::in | ios::out);
 	string str;
-	vector<vector<string>>num;  // vectorÈİÆ÷ÓÃÀ´´æ·ÅÊı¾İ£¨Ã¿Ò»ĞĞÊı¾İÎªÒ»¸övector£©£¬Í³Ò»°´stringĞÍÊı¾İ¶ÁÈ¡
+	vector<vector<string>>num;  // vectorÃˆÃÃ†Ã·Ã“ÃƒÃ€Â´Â´Ã¦Â·Ã…ÃŠÃ½Â¾ÃÂ£Â¨ÃƒÂ¿Ã’Â»ÃÃÃŠÃ½Â¾ÃÃÂªÃ’Â»Â¸Ã¶vectorÂ£Â©Â£Â¬ÃÂ³Ã’Â»Â°Â´stringÃÃÃŠÃ½Â¾ÃÂ¶ÃÃˆÂ¡
 
-	//  °´ĞĞ¶ÁÈ¡ÎÄ¼ş
+	//  Â°Â´ÃÃÂ¶ÃÃˆÂ¡ÃÃ„Â¼Ã¾
 	while (getline(inFile, str))
 	{
 		istringstream input(str);
-		
-		vector<string> tmp;  // ÁÙÊ±vectorÓÃÀ´´æ·ÅÃ¿ĞĞÊı¾İ
+
+		vector<string> tmp;  // ÃÃ™ÃŠÂ±vectorÃ“ÃƒÃ€Â´Â´Ã¦Â·Ã…ÃƒÂ¿ÃÃÃŠÃ½Â¾Ã
 		string a;
 		while (input >> a)
 		{
 			tmp.push_back(a);
 		}
 		//tmp.push_back("MAD\tCV");
-		num.push_back(tmp);	
+		num.push_back(tmp);
 	}
 
-	
-	float sum[21];
-	for (int i = 0; i < sizeof(sum) / sizeof(sum[0]); i++)
+
+	float *sum=new float [num[0].size()];//[SUMARRSIZE];
+	/*for (int i = 0; i < sizeof(sum) / sizeof(sum[0]); i++)
 	{
 		sum[i] = 0;
-	}
-
-	for (int j = 0; j < sizeof(sum) / sizeof(sum[0]); j++)
+	}*/
+	/*for(int i=0;i<num.size();i++)
+	{
+		for(int j=0;j<num[i].size();j++)
+		{
+			cout<<num[i][j]<<"\t";
+		}
+		cout<<endl;
+	}*/
+	for (int j = 1; j < num[j].size();j++)//sizeof(sum) / sizeof(sum[0]); j++)
 	{
 		for (int i = 1; i < num.size(); i++)
 		{
-			float aa = (float)atof(num[i][j + 1].c_str());
-			sum[j] += aa;
+			float aa = (float)atof(num[i][j].c_str());
+			sum[j-1] += aa;
 		}
 	}
 
+	/*for(int i=0;i<sizeof(sum);i++)
+	{
+		cout<<i<<"="<<sum[i]<<endl;
+	}*/
 	for (int i = 1; i < num.size(); i++)
 	{
 		for (int j = 1; j < num[i].size(); j++)
 		{
-			float a =(float) atof(num[i][j].c_str());
+			float a = (float)atof(num[i][j].c_str());
 			float db2 = a * 1000000 / (sum[j - 1]);  // TPM
 			ostringstream oss;
-			oss.setf(ios::fixed);  // °´¹Ì¶¨µã·½Ê½ÏÔÊ¾¸¡µãÊı
-			oss.precision(20);  // ±£Áô20Î»Ğ¡Êı
+			oss.setf(ios::fixed);  // Â°Â´Â¹ÃŒÂ¶Â¨ÂµÃ£Â·Â½ÃŠÂ½ÃÃ”ÃŠÂ¾Â¸Â¡ÂµÃ£ÃŠÃ½
+			oss.precision(20);  // Â±Â£ÃÃ´20ÃÂ»ÃÂ¡ÃŠÃ½
 
 			oss << db2;
 
 			string str(oss.str());
 			num[i][j] = str;
 		}
-		
+
 	}
 
 	std::ofstream outFile;
-	//  ´ò¿ªĞ´ÈëÎÄ¼şÃû£¬½«vectorÖĞÊı¾İĞ´ÈëÎÄ¼ş
+	//  Â´Ã²Â¿ÂªÃÂ´ÃˆÃ«ÃÃ„Â¼Ã¾ÃƒÃ»Â£Â¬Â½Â«vectorÃ–ÃÃŠÃ½Â¾ÃÃÂ´ÃˆÃ«ÃÃ„Â¼Ã¾
 	outFile.open(dstFile);
 	for (int i = 0; i < num.size(); i++)
 	{
@@ -144,269 +157,397 @@ void FileOperate::GenToTPM(string srcFile, string dstFile)
 
 	inFile.close();
 	outFile.close();
-}
 
-/*¿¿¿¿¿
- *1. ¿¿¿¿¿¿¿¿¿¿tmpArrayMed¿¿
- *2. ¿¿MAD¿CV¿
- *3. ¿¿¿¿¿tmpArrayMed¿¿
- *4. ¿¿MAD¿CV¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿-1
- *5. ¿tmpArrayMed¿¿¿¿¿¿¿¿-1¿¿¿¿¿¿¿*/
-void FileOperate::DataFilter(string srcFile, string dstFile, string op,float val)
+}
+void FileOperate::DataFilter(string srcFile, string dstFile, string op, float val)
 {
 	ifstream inFile;
 	inFile.open(srcFile, ios::in | ios::out | ios::app);
-	string str;  // ¿¿¿¿¿¿¿¿
-	vector<vector<string>>num;  // vectorÈİÆ÷ÓÃÀ´´æ·ÅÊı¾İ£¨Ã¿Ò»ĞĞÊı¾İÎªÒ»¸övector£©£¬Í³Ò»°´stringĞÍÊı¾İ¶ÁÈ¡
-	vector<vector<float>>array(49);
-	vector<vector<float>>arraySort(49);
-	vector<vector<string>>store; //¿¿¿¿¿¿¿¿¿¿¿¿¿
-	for(int i=0;i<arraySort.size();i++)
-	{
-		arraySort[i].resize(23);
-		array[i].resize(23);
-	}
-	int genNum[49] = { 0 };  // Í³¼Æ»ùÒòÊıÁ¿
-	//  °´ĞĞ¶ÁÈ¡ÎÄ¼ş
+	string str;  // Â¿Â¿Â¿Â¿Â¿Â¿Â¿Â¿
+	vector<vector<string>>num;  //Â¿Â¿Â¿Â¿
+	vector<vector<float>>array;// (49);
+	vector<vector<float>>arraySort;// (49);//Â¿Â¿Â¿Â¿Â¿Â¿Â¿Â¿Â¿Â¿Â¿Â¿Â¿Â¿Â¿Â¿Â¿Â¿Â¿
+	vector<vector<string>>store; // Â¿Â¿Â¿Â¿Â¿Â¿Â¿Â¿Â¿Â¿Â¿
+	
+	//int genNum[49] = { 0 };  // ÃÂ³Â¼Ã†Â»Ã¹Ã’Ã²ÃŠÃ½ÃÂ¿
+	//int genNum[] = { 0 };  // ÃÂ³Â¼Ã†Â»Ã¹Ã’Ã²ÃŠÃ½ÃÂ¿
+
+	//  Â°Â´ÃÃÂ¶ÃÃˆÂ¡ÃÃ„Â¼Ã¾
 	while (getline(inFile, str))
 	{
-		for(char &c : str)
+		for (char& c : str)
 		{
-			if(c=='\t')
+			if (c == '\t')
 			{
-				c==' ';
+				c == ' ';
 			}
 		}
-		str.erase(0,str.find_first_not_of(" "));
-		str.erase(str.find_last_not_of(" "+1));	
+		str.erase(0, str.find_first_not_of(" "));
+		str.erase(str.find_last_not_of(" " + 1));
 		istringstream input(str);
-		vector<string> tmp;  // ÁÙÊ±vectorÓÃÀ´´æ·ÅÃ¿ĞĞÊı¾İ
+		vector<string> tmp;  // ÃÃ™ÃŠÂ±vectorÃ“ÃƒÃ€Â´Â´Ã¦Â·Ã…ÃƒÂ¿ÃÃÃŠÃ½Â¾Ã
 		vector<string>tmp1;
 		string a;
 		while (input >> a)
-		{	
+		{
 			tmp.push_back(a);
-		//	tmp1.push_back(a);
+			//      tmp1.push_back(a);
 		}
 		if (op == "MAD")
 		{
 			tmp.push_back("MAD");
-		//	tmp1.push_back("MAD");
+			//      tmp1.push_back("MAD");
 
 		}
 		else if (op == "CV")
 		{
 			tmp.push_back("CV");
-		//	tmp1.push_back("CV");
+			//      tmp1.push_back("CV");
 
 		}
 
 		num.push_back(tmp);
 		store.push_back(tmp);
 	}
-
-	for(int i=1;i<num.size();i++)
+	int *genNum=new int [num.size()];
+	for(int i=0;i<sizeof(genNum);i++)
 	{
-		
-		for(int j=1;j<num[0].size();j++)
+		genNum[i]=0;
+	}
+	arraySort.resize(num.size()-1);
+	array.resize(num.size()-1);
+	for (int i = 0; i < arraySort.size(); i++)
+	{
+		arraySort[i].resize(num[i].size()-1);
+		array[i].resize(num[i].size()-1);
+	}
+	/*for(int i=0;i<num.size();i++)
+	{
+		cout<<i<<endl;
+		for(int j=0;j<num[i].size();j++)
 		{
-			arraySort[i][j]=(float)atof(num[i][j].c_str());
-			array[i][j]=(float)atof(num[i][j].c_str());
-			genNum[i]++;
+			cout<<num[i][j]<<"\t";
 		}
+		cout<<endl;
+	}*/
+	for (int i = 0; i < array.size(); i++)
+	{
+
+		for (int j = 0; j < array[i].size(); j++)
+		{
+			/*if(num[i+1][j+1]!="")
+			{
+				genNum[i]++;
+				//cout<<i<<"="<<genNum[i]<<"\t";
+			}*/
+
+			//cout<<num[i][j]<<"\t";
+			arraySort[i][j] = (float)atof(num[i+1][j+1].c_str());
+			array[i][j] = (float)atof(num[i+1][j+1].c_str());
+		}
+		//cout<<endl;
+
 	}
 
-	for(int i=1;i<arraySort.size();i++)
+	for (int i = 0; i < arraySort.size(); i++)
 	{
-		sort(arraySort[i].begin(),arraySort[i].end()-1);
+		//sort(arraySort[i].begin(), arraySort[i].end() - 1);
+
+		for(int j=0;j<arraySort[i].size();j++)
+		{
+			//cout<<arraySort[i][j]<<"\t";
+			if(arraySort[i][j] or arraySort[i][j]==0)
+			{
+				genNum[i]++;
+				//cout<<i<<"="<<genNum[i]<<"\t";
+			}
+
+		}
+		//cout<<endl;
+		//cout<<i<<"="<<genNum[i]<<endl;
+		sort(arraySort[i].begin(), arraySort[i].end() - 1);
 	}
 
+	int m = num.size();
+	float* median = new float[m];//[49] = { 0 };  // Â´Ã¦Â·Ã…Ã”Â´ÃŠÃ½Â¾ÃÃ–ÃÃÂ»ÃŠÃ½Â¼Â°Ã—Ã®ÂºÃ³Ã‹Ã¹Ã‡Ã³Â¾Ã¸Â¶Ã”Ã–ÃÃÂ»Â²Ã®ÂµÃ„Ã–ÃÃÂ»ÃŠÃ½
+	//CVÃ–Âµ
+	float *cv = new float[m];//[49] = { 0 };
+	float *avg = new float[m];//[49] = { 0 };  // Â´Ã¦Â·Ã…Ã”Â´ÃŠÃ½Â¾ÃÃ†Â½Â¾Ã¹ÃŠÃ½
+	float *sd = new float[m];//[49] = { 0 };  // Â±ÃªÃ—Â¼Ã†Â«Â²Ã® 
+	float *sum = new float[m];//[49] = { 0 };
+	
+	 for(int i=0;i<sizeof(median);i++)
+        {
+                median[i]=0;
+        	cv[i]=0;
+		avg[i]=0;
+		sd[i]=0;
+	}
+	 for(int i=0;i<sizeof(sum);i++)
+	 {
+		 sum[i]=0;
+	 }
 
-	float median[49] = { 0 };  // ´æ·ÅÔ´Êı¾İÖĞÎ»Êı¼°×îºóËùÇó¾ø¶ÔÖĞÎ»²îµÄÖĞÎ»Êı
-	////CVÖµ
-	float cv[49] = { 0 };
-	float avg[49] = { 0 };  // ´æ·ÅÔ´Êı¾İÆ½¾ùÊı
-	float sd[49] = { 0 };  // ±ê×¼Æ«²î 
-	float sum[49] = { 0 };
 
 
-	/*********************MAD¼ÆËã*********************/
-	// ÇóÔ´Êı¾İµÄÖĞÎ»Êı.
-	for (int i = 1; i < arraySort.size();i++)
+
+	//*********************MADÂ¼Ã†Ã‹Ã£********************
+	// Ã‡Ã³Ã”Â´ÃŠÃ½Â¾ÃÂµÃ„Ã–ÃÃÂ»ÃŠÃ½.
+	for (int i = 0; i < arraySort.size(); i++)
 	{
-		int n = genNum[i]+1;
+		int n = genNum[i] - 1;
+		//cout<<i<<"="<<genNum[i+1]<<endl;
 		if (n < 2)
 		{
-			median[i] = arraySort[i][1];
-			
+			median[i] = arraySort[i][0];
 		}
 		if (n % 2 == 0)
 		{
-			
-			median[i] = (arraySort[i][n/2]+arraySort[i][n/2+1])/2;
-			
+
+			// median[i] = arraySort[i][n / 2-1];
+
+			median[i] = (arraySort[i][n / 2-1] + arraySort[i][n / 2 ]) / 2;
+
 		}
 		else
 		{
-			median[i] = arraySort[i][n/2];
-			
+			//median[i] = (arraySort[i][n / 2-1] + arraySort[i][n / 2 ]) / 2;
+
+			median[i] = arraySort[i][n / 2];
 		}
 
+		//cout<<median[i]<<endl;
 	}
-	// ÇóÔ´Êı¾İµÄ¾ø¶ÔÖĞÎ»²î
-	for (int i = 1; i <array.size();i++ )
+	// Ã‡Ã³Ã”Â´ÃŠÃ½Â¾ÃÂµÃ„Â¾Ã¸Â¶Ã”Ã–ÃÃÂ»Â²Ã®
+	for (int i = 0; i < array.size(); i++)
 	{
-		for (int j = 1; j < array[i].size();j++)
+		for (int j = 0; j < array[i].size(); j++)
 		{
 			float aa = array[i][j];
-			float tmpMedian = (float)abs(aa - median[i]);  
-			arraySort[i][j]=tmpMedian;
+			float tmpMedian = (float)abs(aa - median[i]);
+			arraySort[i][j] = tmpMedian;
 		}
-		// ¼ÆËãMAD£º¾ø¶ÔÖĞÎ»²îµÄÖĞ¼äÊı
-		sort(arraySort[i].begin(),arraySort[i].end()-1);
-		int n = genNum[i]+1 ;
+	}
+	/*for(int i=0;i<arraySort.size();i++)
+        {
+                for (int j=0;j<arraySort[i].size();j++)
+                {
+                        cout<<arraySort[i][j]<<"\t";
+                }
+                cout<<endl;
+        }*/
+
+	for(int i=0;i<array.size();i++)
+	{
+
+		sort(arraySort[i].begin(), arraySort[i].end() - 1);
+		int n = genNum[i] - 1;
 		if (n < 2)
 		{
-			median[i] = arraySort[i][1];
+			median[i] = arraySort[i][0];
 
 		}
 		if (n % 2 == 0)
 		{
+			//median[i] = arraySort[i][n / 2-1];
 
-			median[i] = (arraySort[i][n/2]+arraySort[i][n/2+1])/2;
+			median[i] = (arraySort[i][n / 2-1] + arraySort[i][n / 2 ]) / 2;
 
 		}
 		else
 		{
-			median[i] = arraySort[i][n/2];
+			//median[i] = (arraySort[i][n / 2-1] + arraySort[i][n / 2 ]) / 2;
+			median[i] = arraySort[i][n / 2];
 
 		}
+		//cout<<median[i]<<endl;
 	}
-	/********************¼ÆËãCV**********************/
-	float sd1[49] = { 0 };
-	for (int i = 1; i < array.size();i++)
+	/*for(int i=0;i<arraySort.size();i++)
 	{
-		for (int j = 1; j < array[i].size();j++)
+		for (int j=0;j<arraySort[i].size();j++)
+		{
+			cout<<arraySort[i][j]<<"\t";
+		}
+		cout<<endl;
+	}*/
+	
+	float* sd1 = new float[num.size()];//[49] = { 0 };
+	for (int i = 0; i < array.size(); i++)
+	{
+		for (int j = 0; j < array[i].size(); j++)
 		{
 			sum[i] += array[i][j];
 		}
+		//cout<<sum[i]<<endl;
 		avg[i] = sum[i] / (genNum[i]-1);
-
-		for (int j = 1; j < (genNum[i]); j++)
+		//cout<<genNum[i]<<endl;
+		//cout<<avg[i]<<endl;
+		for (int j = 0; j < (genNum[i]-1); j++)
 		{
-			sd1[i] +=(float) pow(array[i][j]-avg[i],2);
+			sd1[i] += (float)pow(array[i][j] - avg[i], 2);
 		}
-		sd[i] = sqrt(sd1[i] / (genNum[i] - 2))/avg[i];
-		if(avg[i]==0)
+
+		//sd[i] = sqrt(sd1[i] / (genNum[i] -1)) / avg[i];
+		if (avg[i] == 0)
 		{
-			sd[i]=-1;
+			sd[i] = -1;
+			//cout<<"ERROR!!!"<<endl;
+		}
+		else
+		{
+			sd[i] = sqrt(sd1[i] / (genNum[i] -2)) / avg[i];
+
 		}
 	}
-	
-	for (int i = 1; i < array.size();i++)
+
+
+	for (int i = 0; i < array.size(); i++)
 	{
-		for (int j = genNum[i]; j < array[i].size();j++)
+		//cout<<i<<"="<<array[i].size()<<endl;
+		for (int j = genNum[i]-1; j < array[i].size(); j++)
 		{
-			if (op=="MAD")
+			if (op == "MAD")
 			{
-				array[i][j]=median[i];
+				array[i][j] = median[i];
 			}
 			else
 			{
-				array[i][j]=sd[i];
+				array[i][j] = sd[i];
 			}
+		}
+	}
+
+	/*for(int i=0;i<array.size();i++)
+        {
+                for (int j=0;j<array[i].size();j++)
+                {
+                        cout<<array[i][j]<<"\t";
+                }
+                cout<<endl;
+        }*/
+
+	for (int i=0;i<array.size();i++)
+	{
+		if(array[i][array[i].size()-1]<val)
+		{
+			num[i+1][0]="-1";
+			store[i+1][0]="-1";
 		}
 	}
 	for (int i = 1; i < num.size(); i++)
 	{
 		for (int j = 1; j < num[i].size(); j++)
 		{
-			
+
+
 			ostringstream oss;
-			oss.precision(20);  
+			oss.precision(20);
 			if (op == "MAD")
 			{
+
+				
 				if (j == genNum[i])
 				{
 					oss.precision(2);
 
-					if (array[i][j]<val)
+					if (array[i-1][j-1] < val)
 					{
 						num[i][0] = "-1";
-
+						store[i][0]="-1";
 					}
 				}
+				float db2 = array[i-1][j-1];
+                        	oss.setf(ios::fixed);
+                        	oss << db2;
+                       		string str(oss.str());
+                        	num[i][j] = str;
+                        	store[i][j]=str;
+
 			}
-			else if(op=="CV")
+			else if (op == "CV")
 			{
+				 //cout<<"here  CV"<<endl;
+				//cout<<"genNum"<<i<<"="<<genNum[i]<<endl;
 				if (j == genNum[i])
 				{
 					oss.precision(2);
-
-					if (array[i][j]<val)
+	
+					if (array[i-1][j-1] < val)
 					{
 						num[i][0] = "-1";
-
+						store[i][0]="-1";
 					}
 				}
+				float db2 = array[i-1][j-1];
+                        	oss.setf(ios::fixed);
+                       		oss << db2;
+                        	string str(oss.str());
+                	        num[i][j] = str;
+				store[i][j]=str;
+	
 			}
-			float db2 = array[i][j];
-			oss.setf(ios::fixed); 
-			oss << db2;
-			string str(oss.str());
-			num[i][j] = str;
-			//store[i][j]=str;
 		}
 	}
+
+	
 	std::ofstream outFile;
-	//  ´ò¿ªĞ´ÈëÎÄ¼şÃû£¬½«vectorÖĞÊı¾İĞ´ÈëÎÄ¼ş
+	//  Â´Ã²Â¿ÂªÃÂ´ÃˆÃ«ÃÃ„Â¼Ã¾ÃƒÃ»Â£Â¬Â½Â«vectorÃ–ÃÃŠÃ½Â¾ÃÃÂ´ÃˆÃ«ÃÃ„Â¼Ã¾
 	outFile.open(dstFile);
-	int tag=0;
+	int tag = 0;
 	int size=store.size();
-	vector<string>tmpStore;
-	for(int i=0;i<num.size();i++)
+	for (int i = 0; i < num.size(); i++)
 	{
-		for(int j=0;j<num[i].size();j++)
+		for (int j = 0; j < num[i].size(); j++)
 		{
-			if(num[i][0]=="-1")
+			if (num[i][0] == "-1")
 			{
-				cout<<"num"<<i<<"="<<"-1"<<endl;;
+				//cout << "num" << i << "=" << "-1" << endl;;
 				tag++;
 				size--;
 				break;
 			}
 			else
 			{
-				cout<<"num"<<i<<j<<"="<<num[i][j]<<endl;
-				store[i-tag].push_back(num[i][j]);
+				//store[i-tag][j]=num[i][j];
+				//cout << "num" << i << j << "=" << num[i][j] << endl;
+				store[i - tag].push_back(num[i][j]);
 			}
-			
+
 
 		}
-		cout<<store[i-tag].size()<<endl;	
+		//cout << store[i - tag].size() << endl;
 	}
-	cout<<endl<<endl;
-	/*for(int i=0;i<size;i++)
+	cout << endl << endl;
+	for(int i=0;i<size;i++)
 	{
-		for(int j=0;j<store[i].size();j++)
+		for(int j=0;j<store[i].size()-num[i].size();j++)
 		{
-			cout<<store[i][j]<<"\t";
+			cout<<store[i][j+num[i].size()]<<"\t";
 		}
 		cout<<endl;
-	}*/
-
+	}
 	for (int i = 0; i < size; i++)
 	{
-		for (int j = 0; j < store[i].size()-23; j++)
-		{	
-	
-			outFile << store[i][j+23] << "\t";
+	//	if(store[i][0]!="-1")
+		{
 
+
+			for (int j = 0; j < store[i].size()-num[i].size() ; j++)
+			{
+
+				outFile << store[i][j +num[i].size()] << "\t";
+			}
 		}
 
 		outFile << endl;
 	}
-
+	delete[] cv;
+	delete[] avg;
+	delete[] sd;
+	delete[] sd1;
+	delete[] sum;
+	delete[]genNum;
 	inFile.close();
 	outFile.close();
+	//	cout<<"break"<<endl;
 }
